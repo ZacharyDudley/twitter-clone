79971392e4bc13333c7229dd8d0aa0e2f1f407c9
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-09-18 17:17:40 -0500

    added Morgan, made app.js the start script

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 
+app.use(morgan('dev'));
+
+app.use('/special', function (req, res, next) {
+  console.log(req.method, req.url, 'Special Area');
+  res.send('Not 404');
+  next();
+})
 
 app.get('/', function (req, res) {
-  console.log(req.method, req.url);
   res.send('Nothing here.');
 });
 
